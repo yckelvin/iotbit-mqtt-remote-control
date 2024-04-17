86fe516d77931a43e3155cfc95294a07fc59935d
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-18 02:01:44 +0800

    remove ssid password

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    ESP8266_IoT.publishMqttMessage(convertToText(randint(1, 4)), "myhome/1b20/remote-control", ESP8266_IoT.QosList.Qos2)
+    ESP8266_IoT.publishMqttMessage(convertToText(randint(1, 4)), "myhome/9b00/remote-control", ESP8266_IoT.QosList.Qos2)
 })
-ESP8266_IoT.MqttEvent("myhome/1b20/remote-control", ESP8266_IoT.QosList.Qos2, function (message) {
+ESP8266_IoT.MqttEvent("myhome/9b00/remote-control", ESP8266_IoT.QosList.Qos2, function (message) {
     if ((0 as any) == ("1" as any)) {
         OLED.writeStringNewLine("Turn on the light")
     } else if ((0 as any) == ("2" as any)) {
@@ -14,7 +14,7 @@ ESP8266_IoT.MqttEvent("myhome/1b20/remote-control", ESP8266_IoT.QosList.Qos2, fu
 })
 basic.showNumber(0)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("KLHOME", "127214529")
+ESP8266_IoT.connectWifi("ssid", "password")
 let clientID = randint(0, 99999999)
 basic.showNumber(1)
 ESP8266_IoT.setMQTT(
